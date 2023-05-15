20d46486f78490b0ac3ecc24ef8ed927ccb97b06
dabeenkim <ekqls6812@naver.com>
2023-05-16 00:18:49 +0900

    [fix]movie서비스와 레포짓토리 리펙토링#39

--- modules/redisConnector.js
+++ modules/redisConnector.js
@@ -1,59 +1,59 @@
-const redis = require("redis");
-const logger = require("../middlewares/logger");
-const dotenv = require("dotenv");
-dotenv.config();
+// const redis = require("redis");
+// const logger = require("../middlewares/logger");
+// const dotenv = require("dotenv");
+// dotenv.config();
 
-class RedisConnector {
-  constructor() {
-    if (RedisConnector.instance instanceof RedisConnector) {
-      return RedisConnector.instance;
-    }
-    this._setRedis();
-  }
+// class RedisConnector {
+//   constructor() {
+//     if (RedisConnector.instance instanceof RedisConnector) {
+//       return RedisConnector.instance;
+//     }
+//     this._setRedis();
+//   }
 
-  // connection close
-  quit(callback) {
-    this.client.quit(callback);
-  }
+//   // connection close
+//   quit(callback) {
+//     this.client.quit(callback);
+//   }
 
-  _setRedis() {
-    this._setRedisClient();
+//   _setRedis() {
+//     this._setRedisClient();
 
-    //connect 성공
-    this.client.on("connect", this._connectHandler);
+//     //connect 성공
+//     this.client.on("connect", this._connectHandler);
 
-    //connect 성공
-    this.client.on("error", this._errorHandler);
+//     //connect 성공
+//     this.client.on("error", this._errorHandler);
 
-    //connect 성공
-    this.client.on("end", this._endHandler);
+//     //connect 성공
+//     this.client.on("end", this._endHandler);
 
-    this.client.connect().then();
-  }
+//     this.client.connect().then();
+//   }
 
-  _errorHandler(err) {
-    logger.error("\n#### Redis connection Error. >> ", err);
-  }
+//   _errorHandler(err) {
+//     logger.error("\n#### Redis connection Error. >> ", err);
+//   }
 
-  _endHandler() {
-    logger.info("#### Redis connection close. ####");
-  }
+//   _endHandler() {
+//     logger.info("#### Redis connection close. ####");
+//   }
 
-  _connectHandler() {
-    RedisConnector.instance = this;
-    logger.info("#### Redis connection. ####");
-  }
+//   _connectHandler() {
+//     RedisConnector.instance = this;
+//     logger.info("#### Redis connection. ####");
+//   }
 
-  _setRedisClient() {
-    this.client = redis.createClient({
-      url: `redis://:${process.env.REDIS_PASSWORD}@${process.env.REDIS_HOST}:${process.env.REDIS_PORT}/0`,
-      legacyMode: true,
-    });
-  }
+//   _setRedisClient() {
+//     this.client = redis.createClient({
+//       url: `redis://:${process.env.REDIS_PASSWORD}@${process.env.REDIS_HOST}:${process.env.REDIS_PORT}/0`,
+//       legacyMode: true,
+//     });
+//   }
 
-  getClient() {
-    return this.client.v4;
-  }
-}
+//   getClient() {
+//     return this.client.v4;
+//   }
+// }
 
-module.exports = new RedisConnector();
+// module.exports = new RedisConnector();
--- services/movie.service.js
+++ services/movie.service.js
@@ -8,11 +8,13 @@ class MovieService {
   //전체영상 조회
   allMovies = async (viewLimit) => {
     const movie = await this.movieRepository.FindAll(viewLimit);
-    return movie;
-  };
-  allMovies = async (viewLimit) => {
-    const movie = await this.movieRepository.FindAll(viewLimit);
-    return movie;
+    const filteredMovies = movie.filter((movies) => {
+      return (
+        movies.viewLimit === viewLimit ||
+        (viewLimit === "VL000001" && movies.viewLimit === "VL000002")
+      );
+    });
+    return filteredMovies;
   };
 
   //영상 카테고리 전달
@@ -33,8 +35,21 @@ class MovieService {
       genre,
       viewLimit
     );
+    if (!videoList.findGenre) {
+      throw new Error(`No matching genre: ${genre}`);
+    }
+    const categoryList = await this.movieRepository.findCategoryList(videoList);
+    const filteredVideos = findCategory.filter((movie) => {
+      return (
+        movie.viewLimit === viewLimit ||
+        (viewLimit === "VL000001" && movie.viewLimit === "VL000002")
+      );
+    });
 
-    return videoList;
+    return {
+      Genre: videoList.dataValues.codename,
+      videos: filteredVideos,
+    };
   };
 
   //영상 상세조회
@@ -43,14 +58,19 @@ class MovieService {
     if (!movie) {
       throw Boom.notFound("영상이 존재하지 않습니다.", false);
     }
-    return movie;
-  };
-  onesMovie = async (contentIdx) => {
-    const movie = await this.movieRepository.FindOne(contentIdx);
-    if (!movie) {
-      throw Boom.notFound("영상이 존재하지 않습니다.", false);
-    }
-    return movie;
+    const [findPerson, findOnesMovie, findAllPerson] = movie;
+    const personCodes = findPerson
+      .filter((p) => {
+        const codeValue = findAllPerson.find(
+          (fp) => fp.person === p.dataValues.codeValue
+        );
+        return codeValue !== undefined;
+      })
+      .map((p) => p.dataValues.codename);
+    return {
+      Video: findOnesMovie,
+      actor: personCodes,
+    };
   };
 
   //찜목록 조회
@@ -62,7 +82,13 @@ class MovieService {
     if (!category) {
       throw Boom.notFound("찜목록이 존재하지 않습니다.", false);
     }
-    return category;
+    const filteredVideos = category.filter((movie) => {
+      return (
+        movie.viewLimit === viewLimit ||
+        (viewLimit === "VL000001" && movie.viewLimit === "VL000002")
+      );
+    });
+    return filteredVideos;
   };
 
   //viewRank순 조회
@@ -71,7 +97,20 @@ class MovieService {
     if (!category) {
       throw Boom.notFound("카테고리가 존재하지 않습니다.", false);
     }
-    return category;
+    const filteredVideos = category.filter((movie) => {
+      return (
+        movie.viewLimit === viewLimit ||
+        (viewLimit === "VL000001" && movie.viewLimit === "VL000002")
+      );
+    });
+
+    const rankedVideos = filteredVideos.map((movie, index) => {
+      return {
+        ...movie,
+        rank: index + 1,
+      };
+    });
+    return rankedVideos;
   };
 
   //likeRank순 조회
@@ -80,7 +119,20 @@ class MovieService {
     if (!category) {
       throw Boom.notFound("카테고리가 존재하지 않습니다.", false);
     }
-    return category;
+    const filteredVideos = category.filter((movie) => {
+      return (
+        movie.viewLimit === viewLimit ||
+        (viewLimit === "VL000001" && movie.viewLimit === "VL000002")
+      );
+    });
+
+    const rankedVideos = filteredVideos.map((movie, index) => {
+      return {
+        ...movie,
+        rank: index + 1,
+      };
+    });
+    return rankedVideos;
   };
 
   /**
@@ -89,9 +141,7 @@ class MovieService {
    * @returns 내가 본 영상 배열
    */
   viewHistory = async (profileIdx) => {
-    const category = await this.movieRepository.viewHistory(
-      profileIdx
-    );
+    const category = await this.movieRepository.viewHistory(profileIdx);
     if (!category) {
       throw Boom.notFound("카테고리가 존재하지 않습니다.", false);
     }
@@ -104,14 +154,12 @@ class MovieService {
    * @returns 영상 정보 contentIdx, name, videoUrl, videoThumUrl
    */
   viewContent = async (contentIdx) => {
-    const viewContent = await this.movieRepository.viewContent(
-      contentIdx
-    )
+    const viewContent = await this.movieRepository.viewContent(contentIdx);
     if (!viewContent) {
       throw Boom.notFound("영상이 존재하지 않습니다.", false);
     }
     return viewContent;
-  }
+  };
 
   /**
    * 영상 조회 횟수 증가
@@ -121,11 +169,12 @@ class MovieService {
    */
   viewIncrease = async (profileIdx, contentIdx) => {
     const viewIncrease = await this.movieRepository.viewIncrease(
-      profileIdx, contentIdx
-    )
+      profileIdx,
+      contentIdx
+    );
     return viewIncrease;
-  }
-  
+  };
+
   /**
    * 내가 본 영상 기록 남기기
    * @param {String} profileIdx
@@ -134,10 +183,11 @@ class MovieService {
    */
   viewRecordHistory = async (profileIdx, contentIdx) => {
     const viewRecordHistory = await this.movieRepository.viewRecordHistory(
-      profileIdx, contentIdx
-    )
+      profileIdx,
+      contentIdx
+    );
     return viewRecordHistory;
-  }
+  };
 
   /**
    * 이 컨텐츠 좋아요
@@ -146,17 +196,20 @@ class MovieService {
    * @return 'delete' or 'create'
    */
   pickThisContent = async (profileIdx, contentIdx) => {
-    const pickThisContent = await this.movieRepository.pickThisContent(profileIdx, contentIdx)
+    const pickThisContent = await this.movieRepository.pickThisContent(
+      profileIdx,
+      contentIdx
+    );
 
-    if(pickThisContent === 'create'){
+    if (pickThisContent === "create") {
       // 좋아요 한 경우 likeRank +
-      await this.movieRepository.pickContentIncrease(profileIdx, contentIdx)
-    }else{
+      await this.movieRepository.pickContentIncrease(profileIdx, contentIdx);
+    } else {
       // 좋아요 취소 한 경우 likeRank -
-      await this.movieRepository.pickContentdecrease(profileIdx, contentIdx)
+      await this.movieRepository.pickContentdecrease(profileIdx, contentIdx);
     }
 
-    return pickThisContent
+    return pickThisContent;
   };
 }
 
